0e7d33045c1fbda28ffcdc29c8c4e17d284f66e0
mminsong <105476298+mminsong@users.noreply.github.com>
2024-02-15 14:30:15 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT i.animal_id, i.name
+from animal_ins i ,animal_outs o
+where i.animal_id=o.animal_id(+)
+and i.datetime<=o.datetime
+order by o.datetime-i.datetime desc
+fetch first 2 rows only;
